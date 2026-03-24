e260094b29f8de27c07bb06f8b2c8e125822f04d
Phan Công Thăng <loops0812@duck.com>
2026-03-24 17:52:32 +0700

    feat: implement distance and temperature conversion utilities with error handling

---
+++ src/common/enum/temperature-unit.enum.ts
@@ -0,0 +1,5 @@
+export enum TemperatureUnit {
+  CELSIUS = 'C',
+  FAHRENHEIT = 'F',
+  KELVIN = 'K',
+}
---
+++ src/metrics/converters/temperature.converter.ts
@@ -0,0 +1,44 @@
+import { TemperatureUnit } from 'src/common/enum/temperature-unit.enum';
+import { IUnitConverter } from './types/unit-converter.interface';
+import { UnsupportedTemperatureUnitError } from 'src/common/errors';
+
+/**
+ * Temperature conversion utilities.
+ * All conversions go through Celsius as the base unit.
+ */
+export class TemperatureConverter implements IUnitConverter<TemperatureUnit> {
+  toBase(value: number, unit: TemperatureUnit): number {
+    switch (unit) {
+      case TemperatureUnit.CELSIUS:
+        return value;
+      case TemperatureUnit.FAHRENHEIT:
+        return (value - 32) * (5 / 9);
+      case TemperatureUnit.KELVIN:
+        return value - 273.15;
+      default:
+        throw new UnsupportedTemperatureUnitError(unit);
+    }
+  }
+
+  fromBase(baseValue: number, unit: TemperatureUnit): number {
+    switch (unit) {
+      case TemperatureUnit.CELSIUS:
+        return baseValue;
+      case TemperatureUnit.FAHRENHEIT:
+        return baseValue * (9 / 5) + 32;
+      case TemperatureUnit.KELVIN:
+        return baseValue + 273.15;
+      default:
+        throw new UnsupportedTemperatureUnitError(unit);
+    }
+  }
+
+  convert(
+    value: number,
+    fromUnit: TemperatureUnit,
+    toUnit: TemperatureUnit,
+  ): number {
+    const base = this.toBase(value, fromUnit);
+    return this.fromBase(base, toUnit);
+  }
+}
